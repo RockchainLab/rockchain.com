0538d93c07ed60874d2ee73cba75b789706084ea
Marek Mahdal <marek@rockchain.com>
2018-10-02 11:11:47 +0200

    Added Smart Contracts section

---
+++ smart-contracts.php
@@ -0,0 +1,110 @@
+<?
+$title = "Smart Contract development";
+$custom_meta = '
+<meta name="keywords" content="Smart Contract, Development" />
+<meta name="description" content="Smart contract development for blockchain and other distributed ledger technologies. Utility Tokens (ERC20), Security Tokens, Multi-signature wallets and more. " />
+<meta name="robots" content="index, follow" />';
+
+$custom_og = '
+<meta property="og:url" content="https://www.rockchain.com/"/>
+<meta property="og:title" content="Smart Contract, Development"/>
+<meta property="og:description" content="Smart contract development for blockchain and other distributed ledger technologies. Utility Tokens (ERC20), Security Tokens, Multi-signature wallets and more."/>';
+?>
+<?php include 'header.php'; ?>
+<?php $active = 'smart-contracts'; ?>
+<?php $active_submenu = 'smart-contracts'; ?>
+<section class="section-hero hero-small-illustration hero-illustration-1">
+	<div class="container section-hero__container">
+		<?php include 'menu.php'; ?>
+		<h1 class="heading-1"><span class="highlight">Smart Contracts</span> Development</h1>
+		<nav class="tab-nav tab-nav-4">
+			<div class="tab-nav__tab">
+				<a class="tab-nav__link" href="gbx_sponsor_firm">Smart Contracts</a>
+			</div>
+			<div class="tab-nav__tab active">
+				<a class="tab-nav__link" href="utility-tokens">Utility Tokens</a>
+			</div>
+			<div class="tab-nav__tab">
+				<a class="tab-nav__link" href="asset-backed-tokens">Asset Backed Tokens</a>
+			</div>
+			<div class="tab-nav__tab">
+				<a class="tab-nav__link" href="security-tokens">Security Tokens</a>
+			</div>
+			<div class="tab-nav__tab">
+				<a class="tab-nav__link" href="multisig-wallet">Multi-Sig Wallets</a>
+			</div>
+			<div class="tab-nav__tab">
+				<a class="tab-nav__link" href="bespoke-smart-contracts">Bespoke</a>
+			</div>
+		</nav>
+	</div>
+</section>
+<section class="section-white">
+	<div class="container section-white__container">
+		<nav class="tab-nav-mobile">
+			<div class="tab-nav-mobile__tab active">
+				<a class="tab-nav-mobile__link" href="gbx_sponsor_firm">Smart Contracts</a>
+			</div>
+			<div class="tab-nav-mobile__tab">
+				<a class="tab-nav-mobile__link" href="utility-tokens">Utility Tokens</a>
+			</div>
+			<div class="tab-nav-mobile__tab">
+				<a class="tab-nav-mobile__link" href="asset-backed-tokens">Asset Backed Tokens</a>
+			</div>
+			<div class="tab-nav-mobile__tab">
+				<a class="tab-nav-mobile__link" href="security-tokens">Security Tokens</a>
+			</div>
+			<div class="tab-nav-mobile__tab">
+				<a class="tab-nav-mobile__link" href="multisig-wallet">Multi-Sig Wallets</a>
+			</div>
+			<div class="tab-nav-mobile__tab">
+				<a class="tab-nav-mobile__link" href="bespoke-smart-contracts">Bespoke</a>
+			</div>
+		</nav>
+		<div class="row-3">
+			<div class="box-4 advantage">
+				<div class="advantage__icon advantage__icon-1"></div>
+				<h3 class="heading-3">Smart Contracts</h3>
+				<p class="paragraph-2">Software that facilitates, verifies, or enforces a digital contract running on Blockchain or other forms of Distributed Ledger Technology.</p>
+			</div>
+			<div class="box-4 advantage">
+				<div class="advantage__icon advantage__icon-2"></div>
+				<h3 class="heading-3">Secure</h3>
+				<p class="paragraph-2">Smart contracts enable transactions without reliance on third parties.</p>
+			</div>
+			<div class="box-4 advantage">
+				<div class="advantage__icon advantage__icon-3"></div>
+				<h3 class="heading-3">Transparency</h3>
+				<p class="paragraph-2">Smart Contract transactions are trackable and irreversible.</p>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="section-grey">
+	<div class="container section-grey__container">
+		<h2 class="heading-2">Smart Contract types</h2>
+		<div class="row-3">
+			<div class="box-6">
+				<ul class="point-list">
+					<li>
+						<p>Tokens</p>
+						<p class="paragraph-1 paragraph-1--forced">Tokens are a popular type of smart contract currently used on public blockchains, with Ethereum based - <a href="https://en.wikipedia.org/wiki/ERC-20">ERC20 compatible</a> contracts being the most prevailing. Tokens can be fungible or non-fungible (e.g. <a href="http://erc721.org/">ERC721</a>), perform a valuable utility function in a decentralized application, represent a virtual or physical asset (<a href="asset-backed-tokens">Asset Backed Tokens</a>) or behave as a financial security (<a href="security-tokens">Security Tokens</a>).</p>
+					</li>
+					<li>
+						<p>Multi Signature Wallets</p>
+						<p class="paragraph-1 paragraph-1--forced">Smart Contracts can also be deployed to perform a function of a Multi Signature Wallet.</p>
+					</li>
+					<li>
+						<p>Bespoke Smart Contracts</p>
+						<p class="paragraph-1 paragraph-1--forced">Rockchain can help you develop smart contracts tailored to your specific needs following industry best practices and DevOps management.</p>
+					</li>
+				</ul>
+			</div>
+			<div class="box-6">
+				<p>Smart Contract Audit</p>
+				<p class="paragraph-1">We can audit your Smart Contract code and help mitigate security, integrity and availability issues. To ensure independency of audit, we use reliable 3rd party audit firms to audit any code that we develop for you.</p>
+			</div>
+		</div>
+	</div>
+</section>
+<?php include 'footer.php'; ?>
