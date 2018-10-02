585fc0d64a2135a5164b9a4e4f533c623840106e
Marek Mahdal <marek@rockchain.com>
2018-10-02 07:44:35 +0200

    Rollbacking to the original version for code pipeline.

---
+++ index.php
@@ -0,0 +1,53 @@
+<?
+$title = "Rockchain.com | Advisory and integrity for the Tokenized Economy";
+$custom_meta = '
+<meta name="keywords" content="Rockchain, Tokenization, Tokenized Securities, Advisory, Crypto, DLT, Cryptocurrency, Blockchain, Gibraltar, Singapore, GBX, Sponsor" />
+<meta name="description" content="Rockchain supports businesses and investors in the Tokenized Economy. We leverage our deep understanding of the Blockchain ecosystem and our legal, financial and technical experience of the Distributed Ledger Technology, Digital Asset Management." />
+<meta name="robots" content="index, follow" />';
+
+$custom_og = '
+<meta property="og:url" content="https://www.rockchain.com/"/>
+<meta property="og:title" content="Rockchain.com - Advisory and integrity for the Tokenized Economy."/>
+<meta property="og:description" content="Rockchain supports businesses and investors in the Tokenized Economy, leveraging deep understanding of the Blockchain ecosystem, and our legal, financial and technical experience of the Distributed Ledger Technology and Digital Asset Management."/>';
+?>
+<?php include 'header.php'; ?>
+<?php $active = 'home'; ?>
+<?php $active_submenu = 'home'; ?>
+<section class="section-hero hero-illustration hero-illustration-1">
+	<div class="container section-hero__container">
+		<?php include 'menu.php'; ?>
+		<h1 class="heading-1">Offering advisory and integrity for the <span class="highlight">Tokenized Economy</span>.</h1>
+	</div>
+</section>
+<section class="section-white">
+	<div class="container section-white__container">
+		<h2 class="heading-2">Who we are</h2>
+		<div class="row">
+			<div class="box-6 box-m-12">
+				<p class="paragraph-1">Rockchain supports businesses and investors in version 2.0 of the Token Economy interested in transparency, integrity and a sound token-to-market strategy. We leverage our deep understanding of the Token ecosystem and our legal, financial and technical experience of the Distributed Ledger Technology, Digital Asset Management and fundraising.</p>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="section-grey">
+	<div class="container section-grey__container">
+		<h2 class="heading-2">Our services</h2>
+		<ul class="overline-box">
+			<li><a class="overline-box__link" href="token_sales">Tokenization advisory</a></li>
+			<li><a class="overline-box__link" href="gbx_sponsor_firm">Sponsor Firm: GBX Listing</a></li>
+			<li><a class="overline-box__link" href="evaluation_report">Project Analysis and Due Diligence</a></li>
+<!--
+			<li><a class="overline-box__link" href="crypto_funds">Services for Crypto Funds</a></li>
+			<li><a class="overline-box__link" href="crypto_funds">Digital Asset Management</a></li>
+			<li><a class="overline-box__link" href="investor_services">Investor support</a></li>
+-->
+		</ul>
+	</div>
+</section>
+<section class="section-white">
+    <div class="container section-white__container">
+        <h2 class="heading-2">News</h2>
+        <div class="blog-feed"></div>
+    </div>
+</section>
+<?php include 'footer.php'; ?>
---
+++ post_sale_services.php
@@ -0,0 +1,81 @@
+<?
+$title = "Post ICO Services";
+$custom_meta = '
+<meta name="keywords" content="Rockchain, Post-ICO, Token Sale, Post-Sale, TGE, Post-TGE, Token Generation Event" />
+<meta name="description" content="Successfully completing your token sale presents another set of challenges, such as currency conversion to FIAT currency, listing your token on exchanges and creating a healthy market ecosystem." />
+<meta name="robots" content="index, follow" />';
+
+$custom_og = '
+<meta property="og:url" content="https://www.rockchain.com/"/>
+<meta property="og:title" content="Post ICO Services"/>
+<meta property="og:description" content="Successfully completing your token sale presents another set of challenges, such as currency conversion to FIAT currency, listing your token on exchanges and creating a healthy market ecosystem."/>';
+?>
+<?php include 'header.php'; ?>
+<?php $active = 'token-sales'; ?>
+<section class="section-hero hero-small-illustration hero-illustration-1">
+	<div class="container section-hero__container">
+		<?php include 'menu.php'; ?>
+		<h1 class="heading-1">Services for projects that had <span class="highlight">completed a Token Sale</span>.</h1>
+		<nav class="tab-nav tab-nav-4">
+			<div class="tab-nav__tab">
+				<a class="tab-nav__link" href="token_sales">Token sales</a>
+			</div>
+			<div class="tab-nav__tab">
+				<a class="tab-nav__link" href="token_engagement_process">Engagement process</a>
+			</div>
+			<div class="tab-nav__tab">
+				<a class="tab-nav__link" href="token_smart_contracts">Token Smart Contracts</a>
+			</div>
+			<div class="tab-nav__tab active">
+				<a class="tab-nav__link" href="post_sale_services">Post-Sale services</a>
+			</div>
+		</nav>
+	</div>
+</section>
+<section class="section-white">
+	<div class="container section-white__container">
+		<nav class="tab-nav-mobile">
+			<div class="tab-nav-mobile__tab">
+				<a class="tab-nav-mobile__link" href="token_sales">Token sales</a>
+			</div>
+			<div class="tab-nav-mobile__tab">
+				<a class="tab-nav-mobile__link" href="token_engagement_process">Engagement process</a>
+			</div>
+			<div class="tab-nav-mobile__tab">
+				<a class="tab-nav-mobile__link" href="token_smart_contracts">Token Smart Contracts</a>
+			</div>
+			<div class="tab-nav-mobile__tab active">
+				<a class="tab-nav-mobile__link" href="post_sale_services">Post-Sale services</a>
+			</div>
+		</nav>
+		<h2 class="heading-2">Token Sales: Post-Sale services</h2>
+		<div class="row-3 paragraphs">
+			<div class="box-6 box-m-12">
+				<p class="paragraph-1">Successfully completing your token sale presents another set of challenges, such as currency conversion to FIAT currency, listing your token on exchanges and creating a healthy market ecosystem.</p>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="section-questions">
+	<div class="container section-questions__container">
+		<div class="question-wrapper">
+			<h6 class="question-wrapper__question">OTC Conversion Service</h6>
+			<div class="question-wrapper__answer">
+				<p>With a network of our trusted OTC partners we can help you convert the proceeds collected during the token sale event and convert them into FIAT currencies quickly and with less price slippage, so that you can fund your operations and roadmap.</p>
+			</div>
+		</div>
+		<div class="question-wrapper">
+			<h6 class="question-wrapper__question">Market Making</h6>
+			<div class="question-wrapper__answer">
+				<p>We work with providers of  liquidity and price stabilising services for your newly issued token.  Market making services can be an important strategy to provide liquidity and stability after your token sale. This needs to be set up before your token sale for maximum effect.</p>
+			</div>
+		</div>
+		<div class="question-wrapper">
+			<h6 class="question-wrapper__question">Sponsor</h6>
+			<div class="question-wrapper__answer">
+				<p>If we are also your Sponsor Firm (for a GBX listing) we will continue to be your Sponsor Firm subject to the terms of our engagement and the requirements of the GBX rules from time to time.</p>
+			</div>
+		</div>
+	</div>
+</section>
+<?php include 'footer.php'; ?>
