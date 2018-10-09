713c9e1d676f055836a826b8468172e4037e25f7
Marek Mahdal <marek@rockchain.com>
2018-10-09 16:00:51 +0100

    News section

--- index.php
+++ index.php
@@ -43,23 +43,44 @@ $custom_og = '
     <div class="container section-white__container">
         <h2 class="heading-2">News</h2>
         <div class="blog-feed">
+					
 					<div class="blog-feed__post">
-						<a href="" class="blog-feed__thumbnail" style="background-image: url(i/rc-gbx.jpg);"></a>
+						<a href="https://www.businesstimes.com.sg/opinion/raising-capital-in-the-digital-world" class="blog-feed__thumbnail" style="background-image: url(i/bt.jpg);"></a>
 						<div class="blog-feed__text-wrapper">
-							<h3 class="blog-feed__heading"><a class="blog-feed__heading-link" href="">Rockchain announced as GBX Sponsor</a></h3>
-							<div class="blog-feed__metadata">By Rockchain on May 2nd 2018</div>
-							<div class="blog-feed__copy">Rockchain was announced today as a Sponsor Firm approved by the Gibraltar Blockchain Exchange (GBX). The GBX aims to be a world-leading institutional-grade token sale platform and cryptocurrency exchange.
-								<br /><a href="news/rockchain-announced-gbx-sponsor.php">Read the Rockchain GBX Sponsor announcment here.</a></div>
+							<h3 class="blog-feed__heading"><a class="blog-feed__heading-link" href="https://www.businesstimes.com.sg/opinion/raising-capital-in-the-digital-world">Our CEO in The Business Times: Raising-capital-in-the-digital-world</a></h3>
+							<div class="blog-feed__metadata">By Rockchain on September 27th 2018</div>
+							<div class="blog-feed__copy">Our CEO, Julian Jarvis shares with The Business Times, that the potential is great for SMEs and large businesses alike to explore <a href="security-token-offering">Security Token Offerings</a> to fund growth and the roll-out of new products and services.
+								<p><br /><a href="https://www.businesstimes.com.sg/opinion/raising-capital-in-the-digital-world">Read more in The Business Times here.</a></p></div>
 						</div>
 					</div>
 
 					<div class="blog-feed__post">
-						<a href="" class="blog-feed__thumbnail" style="background-image: url(i/rc-gbx.jpg);"></a>
+						<a href="https://medium.com/rockchain-com/rht-holdings-and-rockchain-com-jv-66c3534ec330" class="blog-feed__thumbnail" style="background-image: url(i/rhtra-logo.png);"></a>
 						<div class="blog-feed__text-wrapper">
-							<h3 class="blog-feed__heading"><a class="blog-feed__heading-link" href="">Rockchain announced as GBX Sponsor</a></h3>
+							<h3 class="blog-feed__heading"><a class="blog-feed__heading-link" href="https://medium.com/rockchain-com/rht-holdings-and-rockchain-com-jv-66c3534ec330">RHT Holdings and Rockchain announce joint venture in Singapore</a></h3>
+							<div class="blog-feed__metadata">By Rockchain on June 19th 2018</div>
+							<div class="blog-feed__copy">Asia’s leading professional services group, RHT Holdings and Gibraltar-based consulting and smart contract development company, Rockchain, today announced the establishment of a joint venture to support businesses and investors tapping into increasing opportunities within the crypto economy.
+								<p><br /><a href="https://medium.com/rockchain-com/rht-holdings-and-rockchain-com-jv-66c3534ec330">Read about the RHT Rockchain JV here.</a></p></div>
+						</div>
+					</div>
+
+					<div class="blog-feed__post">
+						<a href="https://medium.com/rockchain-com/rockchain-com-announced-as-gbx-sponsor-972fc85e9ca" class="blog-feed__thumbnail" style="background-image: url(i/rc-gbx.jpg);"></a>
+						<div class="blog-feed__text-wrapper">
+							<h3 class="blog-feed__heading"><a class="blog-feed__heading-link" href="https://medium.com/rockchain-com/rockchain-com-announced-as-gbx-sponsor-972fc85e9ca">Rockchain announced as GBX Sponsor</a></h3>
 							<div class="blog-feed__metadata">By Rockchain on May 2nd 2018</div>
 							<div class="blog-feed__copy">Rockchain was announced today as a Sponsor Firm approved by the Gibraltar Blockchain Exchange (GBX). The GBX aims to be a world-leading institutional-grade token sale platform and cryptocurrency exchange.
-								<br /><a href="news/2018-05-02_rockchain-announced-gbx-sponsor.php">Read the Rockchain GBX Sponsor announcment here.</a></div>
+								<p><br /><a href="https://medium.com/rockchain-com/rockchain-com-announced-as-gbx-sponsor-972fc85e9ca">Read the Rockchain GBX Sponsor announcment here.</a></p></div>
+						</div>
+					</div>
+
+					<div class="blog-feed__post">
+						<a href="https://medium.com/rockchain-com/rht-holdings-and-rockchain-com-jv-66c3534ec330" class="blog-feed__thumbnail" style="background-image: url(i/organic.jpg);"></a>
+						<div class="blog-feed__text-wrapper">
+							<h3 class="blog-feed__heading"><a class="blog-feed__heading-link" href="https://medium.com/rockchain-com/rht-holdings-and-rockchain-com-jv-66c3534ec330">Notes on Utility Token design</a></h3>
+							<div class="blog-feed__metadata">By Rockchain on May 2nd 2018</div>
+							<div class="blog-feed__copy">Rockchain's CTO, Marek Mahdal details his views of the role of Utility Tokens in the Tokenized Economy, value derivation, token velocity and popular Utility Token models today.
+								<p><br /><a href="https://medium.com/rockchain-com/rht-holdings-and-rockchain-com-jv-66c3534ec330">Utility Token Design on Medium.com </a></p></div>
 						</div>
 					</div>
 				</div>
