e5e8b194bf46af1ed8dee9236c0eb05b8d437a5c
Marek Mahdal <marek@rockchain.com>
2018-10-10 08:18:33 +0100

    news formatting and robots.txt

--- news/2018-05-02_Rockchain-announced-gbx-sponsor.php
+++ news/2018-05-02_Rockchain-announced-gbx-sponsor.php
@@ -18,27 +18,23 @@ $custom_og = '
 </section>
 <section class="section-white">
 	<div class="container section-white__container">
-		<h1>Rockchain became an Approved Sponsor Firm of the GBX</h1>
-    <img src="/i/rc-gbx.jpg">
-    <div class="box-6">
-    <p class="paragraph-1">Rockchain Limited (rockchain.com) was announced today as a Sponsor Firm approved by the Gibraltar Blockchain Exchange (GBX). The GBX aims to be a world-leading institutional-grade token sale platform and cryptocurrency exchange.</p>
-    <br />
-    <p class="paragraph-1">Token Sales have become a dominant way to raise funds. Combining the community focused low barrier fundraising of a crowdfunding campaign and, by leveraging the power of the blockchain, Token Sales give early stage technology businesses access to funding whilst simultaneously building a consumer base and enabling the development of disruptive initiatives.</p>
-    <br />
-    <p class="paragraph-1">Julian Jarvis, Rockchain’s co-founder and CEO said, ‘we are proud to be a part of version 2.0 of the crypto-economy, where the exciting and democratising power of crowdfunding meets sensible levels of regulation and due diligence.’</p>
-    <br />
-    <p class="paragraph-1">The GBX requires every Token Issuer listing on the exchange to subscribe to the highest standards in meeting its core principles of ‘Security, Transparency and Accessibility’ and relies on Sponsor Firms to uphold these standards. Sponsor Firms are integral to the listing process — they are responsible for preparing applications, guiding listings to market, and meeting continuing obligations post sale.</p>
-    <br />
-    <p class="paragraph-1">The GBX employ a rigorous approval process, and only high-quality firms are anticipated to be admitted as Sponsor Firms. Rockchain contains advisors and entrepreneurs with backgrounds in Distributed Ledger Technology, egaming, software development, media, law, investment banking and finance.</p>
-    <br />
-    <p class="paragraph-1">Mr Jarvis went on to say ‘We have a high-quality team with great experience. A GBX listing requires exacting standards which sit firmly alongside our own principles of transparency, accountability and integrity. Our passion is to work with entrepreneurs and innovators to help them realise their vision and to play a role in delivering the standards deserved by investors in this sector.”</p>
-    <br />
-    <p class="paragraph-1">Rockchain partners with best-in-class service providers to deliver the right solution for its clients, rather than simply following a template approach, and looks to support strong use-cases for technology, products and services that will really make a difference.</p>
-    <br />
-    <p class="paragraph-1">Marek Mahdal, Rockchain’s co-founder and Chief Information and Technology Officer says, ‘we are passionate about the role that well-conceived products and platforms can play in leveraging Distributed Ledger Technology, not just the fundraising opportunities that token sales can represent.’</p>
-    <br />
-    <p class="paragraph-1">Rockchain (rockchain.com) is a consulting and crypto fund management business in the exciting and incredibly dynamic Crypto Economy. We believe the opportunities for organisations who can successfully leverage the technology and increasing investment in this space are enormous. We are consulting on a selective basis, choosing quality over quantity in terms of our clients and partners and with a view to building a portfolio of interests that create synergy value for participants. Rockchain’s HQ is in Gibraltar and we are proud to be approved as a Sponsor Firm within the world-leading Token Sale Platform and Cryptocurrency Exchange.</p>
-</div>
+		<div class="blog-article">
+			<h1>Rockchain became an Approved Sponsor Firm of the GBX</h1>
+			<p class="blog-article__quote-author">Published on May 2nd 2018</p>
+			<img src="/i/rc-gbx-large.jpg">
+			<p><a href="https://rockchain.com/">Rockchain Limited</a> was announced today as a Sponsor Firm approved by the <a href="https://gbx.gi/" target="_blank">Gibraltar Blockchain Exchange</a> (GBX). The GBX aims to be a world-leading institutional-grade token sale platform and cryptocurrency exchange.</p>
+			<p>Token Sales have become a dominant way to raise funds. Combining the community focused low barrier fundraising of a crowdfunding campaign and, by leveraging the power of the blockchain, Token Sales give early stage technology businesses access to funding whilst simultaneously building a consumer base and enabling the development of disruptive initiatives.</p>
+			<p class="blog-article__quote">We are proud to be a part of version 2.0 of the crypto-economy, where the exciting and democratising power of crowdfunding meets sensible levels of regulation and due diligence.</p>
+			<p class="blog-article__quote-author"><span>Julian Jarvis</span>, Rockchain’s co-founder and CEO</p>
+			<p>The GBX requires every Token Issuer listing on the exchange to subscribe to the highest standards in meeting its core principles of ‘Security, Transparency and Accessibility’ and relies on Sponsor Firms to uphold these standards. Sponsor Firms are integral to the listing process — they are responsible for preparing applications, guiding listings to market, and meeting continuing obligations post sale.</p>
+			<p>The GBX employ a rigorous approval process, and only high-quality firms are anticipated to be admitted as Sponsor Firms. Rockchain contains advisors and entrepreneurs with backgrounds in Distributed Ledger Technology, egaming, software development, media, law, investment banking and finance.</p>
+			<p class="blog-article__quote">We have a high-quality team with great experience. A GBX listing requires exacting standards which sit firmly alongside our own principles of transparency, accountability and integrity. Our passion is to work with entrepreneurs and innovators to help them realise their vision and to play a role in delivering the standards deserved by investors in this sector.</p>
+			<p class="blog-article__quote-author"><span>Julian Jarvis</span></p>
+			<p>Rockchain partners with best-in-class service providers to deliver the right solution for its clients, rather than simply following a template approach, and looks to support strong use-cases for technology, products and services that will really make a difference.</p>
+			<p class="blog-article__quote">We are passionate about the role that well-conceived products and platforms can play in leveraging Distributed Ledger Technology, not just the fundraising opportunities that token sales can represent.</p>
+			<p class="blog-article__quote-author"><span>Marek Mahdal</span>, Rockchain’s co-founder and Chief Information and Technology&nbsp;Officer</p>
+			<p><a href="https://rockchain.com/">Rockchain</a> is a consulting and crypto fund management business in the exciting and incredibly dynamic Crypto Economy. We believe the opportunities for organisations who can successfully leverage the technology and increasing investment in this space are enormous. We are consulting on a selective basis, choosing quality over quantity in terms of our clients and partners and with a view to building a portfolio of interests that create synergy value for participants. Rockchain’s HQ is in Gibraltar and we are proud to be approved as a Sponsor Firm within the world-leading Token Sale Platform and Cryptocurrency Exchange.</p>
+		</div>
 	</div>
 </section>
 
