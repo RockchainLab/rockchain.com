2f731ba8d0c7e0b48a8ee9ae7ac2c152a64ad3bc
Marek Mahdal <marek@rockchain.com>
2018-10-10 18:49:41 +0100

    async css

--- header.php
+++ header.php
@@ -22,9 +22,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 	<meta name="format-detection" content="telephone=no" />
 
-	<link href="https://fonts.googleapis.com/css?family=Montserrat:700|Open+Sans:400,600,700" rel="stylesheet" async>
-
-	<link rel="stylesheet" type="text/css" href="/scss/main.css" async />
+  <link rel="preload" href="https://fonts.googleapis.com/css?family=Montserrat:700|Open+Sans:400,600,700" as="style" onload="this.rel='stylesheet'">
+  <link rel="preload" href="/scss/main.css" as="style" onload="this.rel='stylesheet'">
 
   <script src="/js/jquery.js" async></script>
 	<script src="/js/functions.js" async></script>
