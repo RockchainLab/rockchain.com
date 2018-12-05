801e82eec57b7b8ca0e0e0dc8de37a6bf6c2e7bd
Marek Mahdal <marek@rockchain.com>
2018-12-05 12:21:50 +0100

    fix typo

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ $custom_og = '
 		<ul class="overline-box">
 			<li><a class="overline-box__link" href="#">Rockproof</a>A privacy-centric timestamping solution, removing reliance on a 3rd party while maintaining irrefutable, publicly verifiable proof of data integrity.</li>
 			<li><a class="overline-box__link" href="http://www.tokentransparency.net" target="_blank">Token Transparency</a>A monitoring service observing token transactions across the Ethereum netowrk.</li>
-			<li><a class="overline-box__link" href="#">Token Testing Facility</a>Validation of Token and other Smart Contracts functionality for compliance with intended prupose and specification.</li>
+			<li><a class="overline-box__link" href="#">Token Testing Facility</a>Validation of Token and other Smart Contracts functionality for compliance with intended purpose and specification.</li>
 		</ul>
 		</div>
 	</div>
