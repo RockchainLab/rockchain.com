d2857f3c62cdeb0697c58ca9bcd7a8ef25eed09a
Marek Mahdal <marek@rockchain.com>
2019-02-13 18:01:01 +0100

    news fix

--- 2018-06-20_RHT-Holdings-and-Rockchain-form-JV.php
+++ 2018-06-20_RHT-Holdings-and-Rockchain-form-JV.php
@@ -10,18 +10,18 @@ $custom_og = '
 <meta property="og:title" content="Rockchain, Gibraltar, RHT, Singapore"/>
 <meta property="og:description" content="RHT Holdings and Rockchain.com announce joint venture dedicated to supporting Asia’s booming crypto economy."/>';
 ?>
-<?php include '../header.php'; ?>
+<?php include 'header.php'; ?>
 <?php $active = 'news'; //in preparatino for a dedicated section accessible from the menu?>
 <?php $active_submenu = ''; ?>
 <section class="section-hero hero-illustration-1">
-	<div class="container"><?php include '../menu.php'; ?></div>
+	<div class="container"><?php include 'menu.php'; ?></div>
 </section>
 <section class="section-white">
 	<div class="container section-white__container">
 		<div class="blog-article">
 			<h1>RHT Holdings and Rockchain.com announce joint venture in Singapore</h1>
 			<p class="blog-article__date">Published on June 20th, 2018</p>
-			<img src="../i/rht-rc.jpg">
+			<img src="i/rht-rc.jpg">
 			<p>Asia’s leading professional services group, RHT Holdings and Gibraltar- based consulting, software development and crypto fund management company, Rockchain.com, today announced the establishment of a joint venture to support businesses and investors tapping into increasing opportunities within the crypto economy.</p>
       <p>Drawing on the combined expertise of RHT Holdings and Rockchain, the new Singapore based joint venture company, RHT Rockchain Advisory Pte Ltd (“RHTRA”), will be an integrated crypto solutions provider, with a firm focus on transparency, integrity and projects with a sound token-to-market strategy.</p>
       <p>RHTRA will deliver blockchain, business consultancy and project management for token generating events (TGEs) amidst the region’s growing interest and investments in the crypto economy.</p>
@@ -41,4 +41,4 @@ $custom_og = '
 	</div>
 </section>
 
-<?php include '../footer.php'; ?>
+<?php include 'footer.php'; ?>
